20efa1557ab028a034a1c4d6d5abe67d832ae4ea
Caio Breno <89470026+CAI0-BREN0@users.noreply.github.com>
2023-05-16 20:59:42 -0400

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,3 @@
+nota1 = float(input('qual foi a sua nota na av1? ' ))
+nota2 = float(input('e qual foi a sua nota na av2? '))
+print('se suas notas são {} na av1 e {} na av2 então sua media sera {}'.format(nota1,nota2,(nota1+nota2)/2))
